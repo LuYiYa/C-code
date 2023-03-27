32168a932d487cac8b381ad0275c39e675687ef9
luyi <2272935758@qq.com>
2023-03-27 08:22:02 +0800

    3—27初识指针

---
+++ 3-26/3-26/test3-26.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main(void)
+//{
+//	int a[10];
+//
+//	printf("%p\n", &a);
+//	printf("%p\n", a);
+//	printf("%p\n", &a[0]);
+//	printf("%p\n", &a[1]);
+//
+//	return 0;
+//}
+ 
+//int main()
+//{
+//	char c;
+//	char d;
+//	c = 1;
+//	d = '1';
+//
+//
+//	return 0;
+//}
+
+int main()
+{
+	printf("123\b\n456\n");
+
+	return 0;
+}
